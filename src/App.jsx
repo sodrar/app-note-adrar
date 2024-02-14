fec971c9cd369467f7a83313c9a15c71d6223b64
BSofian <draaboufianso@gmail.com>
2024-02-14 13:45:16 +0100

    Init du projet

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <h1>TP : Réaliser une application de type Note</h1>
+  )
+}
+
+export default App
