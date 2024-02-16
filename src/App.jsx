d7c8a953fc7e1c48167efab02e2894dc10aa9b12
BSofian <draaboufianso@gmail.com>
2024-02-16 10:56:20 +0100

    Les notes se trie par date, manque à faire remonter en fonction du like

--- src/App.jsx
+++ src/App.jsx
@@ -6,38 +6,7 @@ import { Display } from './components/Display/Display';
 import { Adding } from './components/Adding/Adding';
 function App() {
 
-  const [myNotes, setMyNotes] = useState([
-    {
-      id: 1,
-      title: "Note 1",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      liked: true
-    },
-    {
-      id: 2,
-      title: "Note 2",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      liked: false
-    },
-    {
-      id: 3,
-      title: "Note 3",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      liked: true
-    },
-    {
-      id: 4,
-      title: "Note 4",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      liked: false
-    },
-    {
-      id: 5,
-      title: "Note 5",
-      content: "Lorem ipsum dolor sit amet, consectetur aozejhferfhzerifhzerifzehfzeiuhfdipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      liked: true
-    }
-  ]);
+  const [myNotes, setMyNotes] = useState([]);
 
   const [current, setCurrent] = useState(1);
 
@@ -45,10 +14,6 @@ function App() {
     setCurrent(note)
     console.log(current)
   }
-
-  const notesElements = myNotes.map((note,i) => {
-    return <Note setMyNotes={setMyNotes} note={note} notes={[...myNotes]} key={i} handleSelect={handleSelect} />
-  });
 
   const handleAdd = () => {
     setCurrent(-1);
@@ -64,6 +29,12 @@ function App() {
     setCurrent(1)
   }
 
+  const sortedNote = [...myNotes].sort((a, b) => { return a.date.getTime() - b.date.getTime() })
+
+  const notesElements = sortedNote.map((note, i) => {
+    return <Note setMyNotes={setMyNotes} note={note} notes={[...myNotes]} key={i} handleSelect={handleSelect} />
+  });
+
   return (
     <div className="app-container">
       <div className='notes'>
@@ -72,7 +43,7 @@ function App() {
       </div>
       <div className="display">
         {current === -1 ? <Adding addNote={addNote} /> : <Display current={current} />}
-        
+
       </div>
     </div>
   )
